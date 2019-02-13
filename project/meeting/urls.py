30323148383bfa9f22b979389b96552e166e733e
myeongs14 <audtjr9514@gmail.com>
2019-02-14 01:50:55 +0900

    19.02.14

--- project/meeting/urls.py
+++ project/meeting/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new/', views.new, name="new"),
+    path('new/', views.new, name="new" ),
+    path('create/', views.create, name="create" ),
     path('detail/', views.detail, name="detail"),
-    path('<int:blog>', views.detail, name="detail")
+    path('<int:meet_id>', views.detail, name="detail")
 ]
--- project/meeting/views.py
+++ project/meeting/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Meet
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    meets = Meet.objects
+    return render(request, 'home.html', {'meets': meets})
 
 def new(request):
     return render(request, 'new.html')
 
-def detail(request):
-    return render(request, 'detail.html')
+def create(request):
+    meet = Meet()
+    meet.title = request.GET['title']
+    meet.body = request.GET['body']
+    meet.pub_date = timezone.datetime.now()
+    meet.save()
+    return redirect("/meeting/"+str(meet.id))
+
+def detail(request, meet_id):
+    meet_detail = get_object_or_404(Meet, pk = meet_id)
+    return render(request, 'detail.html', {'meet':meet_detail})
